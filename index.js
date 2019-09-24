6018554cabddeec10d134ec661e901265e977800
Paul M <paulmcadory@gmail.com>
2019-09-24 22:20:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function sayHiToGrandma(string) {
   return "I can't hear you!";
   else if (string === string.toUpperCase())
   return ("YES, INDEED!")
-  else if (string === string.)
+  else if (string === "I love you, Grandma")
 }
