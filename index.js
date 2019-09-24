0b7bd260af0db5a285fb88ca7b2bcdaa48562c42
Paul M <paulmcadory@gmail.com>
2019-09-24 22:19:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase())
-  return "I can't hear you!"
+  return "I can't hear you!";
   else if
   else (string = "console")
 }
