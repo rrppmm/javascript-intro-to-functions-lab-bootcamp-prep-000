c1285d7df7a6a22a841ae73eceee34333602e483
Paul M <paulmcadory@gmail.com>
2019-09-24 22:19:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase())
   return "I can't hear you!";
-  else if
+  else if 
   else if (string = "console")
 }
